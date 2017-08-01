943f0386ac0ac9b7e78be5d4ffba0291b24f4e11
Gina <iamyuchinghsu@gmail.com>
2017-08-01 15:36:31 -0700

    Compare with the T06.01 solution

--- Lesson06-Visualizer-Preferences/T06.01-Exercise-SetupTheActivity/app/src/main/java/android/example/com/visualizerpreferences/AudioVisuals/SettingActivity.java
+++ Lesson06-Visualizer-Preferences/T06.01-Exercise-SetupTheActivity/app/src/main/java/android/example/com/visualizerpreferences/AudioVisuals/SettingActivity.java
@@ -1,8 +1,11 @@
 package android.example.com.visualizerpreferences.AudioVisuals;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.example.com.visualizerpreferences.R;
+import android.support.v7.app.ActionBar;
+import android.view.MenuItem;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        ActionBar actionbar = this.getSupportActionBar();
+
+        //set the action bar back button to look like an up button
+        if(actionbar != null){
+            actionbar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
