98744937081fb7f6d4e2b63a481fe4a486dddcc9
Gina <iamyuchinghsu@gmail.com>
2017-10-29 16:04:02 -0700

    Done T0905-QueryAllTasks

--- Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -121,14 +121,34 @@ public class TaskContentProvider extends ContentProvider {
                         String[] selectionArgs, String sortOrder) {
 
         // TODO (1) Get access to underlying database (read-only for query)
+        final SQLiteDatabase db = mTaskDbHelper.getReadableDatabase();
 
         // TODO (2) Write URI match code and set a variable to return a Cursor
+        int match = sUriMatcher.match(uri);
+        Cursor myCursor;
 
         // TODO (3) Query for the tasks directory and write a default case
+        switch (match){
+            //Query for the tasks directory
+            case TASKS:
+                myCursor =db.query(TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            //Default exception
+            default:
+                throw new UnsupportedOperationException("Unknow uri: "+ uri);
+        }
 
         // TODO (4) Set a notification URI on the Cursor and return that Cursor
+        myCursor.setNotificationUri(getContext().getContentResolver(), uri);
 
-        throw new UnsupportedOperationException("Not yet implemented");
+        //return the desired cursor
+        return myCursor;
     }
 
 
